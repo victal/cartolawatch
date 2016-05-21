9fda8caab6e88fe728dcb1e35734bd05bdeaf3a8
Guilherme Victal <guilhermevictal em gmail ponto com>
2016-05-20 22:12:48 -0300

    Add del and create cleanup task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
-var usemin = require('gulp-usemin');
-var uglify = require('gulp-uglify');
-var minifyHtml = require('gulp-minify-html');
-var minifyCss = require('gulp-minify-css');
-var rev = require('gulp-rev');
+    connect = require('gulp-connect'),
+    usemin = require('gulp-usemin'),
+    uglify = require('gulp-uglify'),
+    minifyHtml = require('gulp-minify-html'),
+    minifyCss = require('gulp-minify-css'),
+    rev = require('gulp-rev'),
+    del = require('del');
 
-gulp.task('server', function(){
-  connect.server({
-    port: 8000,
-    livereload: true
-  });
+gulp.task('server', function () {
+    connect.server({
+        port: 8000,
+        livereload: true
+    });
 });
 
-gulp.task('build', function() {
-  return gulp.src('./*.html')
-  .pipe(usemin({
-    css: [ rev() ],
-    js: [ uglify(), rev() ],
-  }))
-  .pipe(gulp.dest('dist/'));
+
+gulp.task('clean', function () {
+    del.sync('dist/**', '!dist');
+});
+
+gulp.task('build', ['clean'], function () {
+    return gulp.src('./*.html')
+        .pipe(usemin({
+            css: [rev()],
+            js: [uglify(), rev()]
+        }))
+        .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('default', ['build']);
